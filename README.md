f838ac47698c54d86079e906d2ec36d9d6740810
Julio Relosa <Julio@Madalines-MacBook-Air.local>
2024-09-18 19:05:07 -0700

    added 6 more lines of actual code

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# EnvironmentalTracker
+shower1 = input("How many showers do you take per week on average: ")
+shower
--- project1.py
+++ project1.py
@@ -1,1 +1,12 @@
 shower1 = input("How many showers do you take per week on average: ")
+shower2 = input("On average, how long do you shower for this many minutes: ")
+
+#converting input into intergers for math
+shower1 = int(shower1)
+shower2 = int(shower2)
+
+# Weekly shower length math and convert into string to print
+weeklyshower = (shower1 * shower2)
+weeklyshower = str(weeklyshower)
+
+print ("On average, you spend " + weeklyshower + " mintues a week." )
