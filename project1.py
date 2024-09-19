e0d27b6bbae62a1674e180f1e4fd392a2fb9058a
Julio Relosa <Julio@Madalines-MacBook-Air.local>
2024-09-18 19:25:04 -0700

    Added math to see water consumption value. It appears broken, investigate this later.

--- project1.py
+++ project1.py
@@ -1,7 +1,7 @@
 shower1 = input("How many showers do you take per week on average: ")
 shower2 = input("On average, how long do you shower for this many minutes: ")
 
-#converting input into intergers for math
+# Converting input into intergers for math
 shower1 = int(shower1)
 shower2 = int(shower2)
 
@@ -10,3 +10,18 @@ weeklyshower = (shower1 * shower2)
 weeklyshower = str(weeklyshower)
 
 print ("On average, you spend " + weeklyshower + " mintues a week." )
+
+# Weekly water consumption
+# According to EPA, 2.5 gallons of water are spent per minute in a shower
+watergallons = int(2.5) 
+waterweek = (watergallons * weeklyshower)
+waterweek = str(waterweek)
+
+print ("That means you spend roughly " + waterweek + " gallons of water per week on showers alone.")
+
+# Yearly water consumption math
+yearweeks = int(52.143) # weeks per year
+wateryear = (waterweek * yearweeks)
+wateryear = str(wateryear)
+
+print ("That is equivalent to " + wateryear + "gallons per year")
